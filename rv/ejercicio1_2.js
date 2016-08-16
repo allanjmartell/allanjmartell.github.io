cd22d260785c5240bd8d8dff01b6d5f02d70d8d8
allanjmartell <allanjmartell@gmail.com>
2016-08-16 15:42:33 -0500

    Update ejercicio1_2.js

--- rv/ejercicio1_2.js
+++ rv/ejercicio1_2.js
@@ -1,4 +1,4 @@
 var Canvas2=document.getElementById("Canvas2");
 var contexto2=Canvas2.getContext("2d");
 contexto2.fillStyle="blue";
-contexto2.fillRect(60,60,50,50);
+contexto2.fillRect(60,60,100,50);
