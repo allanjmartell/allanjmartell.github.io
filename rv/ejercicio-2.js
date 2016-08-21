01960231eefc21c109ef9af4ca26267bfcee7718
allanjmartell <allanjmartell@gmail.com>
2016-08-20 20:16:42 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -1,7 +1,7 @@
 var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=7;
-camara.position.x=1;
+camara.position.y=1;
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
