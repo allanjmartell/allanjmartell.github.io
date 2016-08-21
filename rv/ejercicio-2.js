e6ac67d24824ad9afaf19f8acb79ad6e37e1311b
allanjmartell <allanjmartell@gmail.com>
2016-08-20 19:51:57 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -6,7 +6,7 @@ renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
 var forma=new THREE.CylinderGeometry(.5,.5,2);
-var forma2=new THREE.SphereGeometry(2,8,6);
+var forma2=new THREE.SphereGeometry(1,8,6);
 
 var material= new THREE.MeshNormalMaterial();
 
