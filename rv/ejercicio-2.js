415a9c8424f42ab4259256e65962f2354ef57386
allanjmartell <allanjmartell@gmail.com>
2016-08-20 19:26:06 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -5,7 +5,7 @@ var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
-var forma=new THREE.CylinderGeometry(1,1,1);
+var forma=new THREE.CylinderGeometry(.5,.5,2);
 var material= new THREE.MeshNormalMaterial();
 var arbol=new THREE.Mesh(forma,material);
 arbol.rotateX(Math.PI/4);
