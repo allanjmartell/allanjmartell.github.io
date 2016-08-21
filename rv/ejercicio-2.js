a4588359ff13e5bb1a6783bb33e00a57a20f9bae
allanjmartell <allanjmartell@gmail.com>
2016-08-20 19:45:08 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -6,16 +6,19 @@ renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
 var forma=new THREE.CylinderGeometry(.5,.5,2);
+var forma2=new THREE.SphereGeometry(.5,3,2);
+
 var material= new THREE.MeshNormalMaterial();
+
 var arbol=new THREE.Mesh(forma,material);
+var arbol2=new THREE.Mesh(forma2,material);
+
 arbol.rotateX(Math.PI/4);
 arbol.rotateY(Math.PI/4);
 
-var forma2=new THREE.SphereGeometry(.5,3,2);
-var arbol2=new THREE.Mesh(forma2,material);
 arbol2.rotateX(Math.PI/4);
 arbol2.rotateY(Math.PI/4);
 
 escena.add(arbol);
-escena.add(arbol2);
+escena.add(arbol2,1,1,1);
 renderizador.render(escena,camara);
