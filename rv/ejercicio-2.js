f263260696cdf0013c3fe07290ec3bf9981a4396
allanjmartell <allanjmartell@gmail.com>
2016-08-20 20:03:14 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -15,9 +15,8 @@ var arbol2=new THREE.Mesh(forma2,material);
 
 arbol.rotateX(Math.PI/4);
 arbol.rotateY(Math.PI/4);
-arbol2.position.set(0,2,0);
-arbol2.rotateX(Math.PI/4);
-arbol2.rotateY(Math.PI/4);
+arbol2.position.set(.5,2,0);
+
 
 escena.add(arbol,arbol2);
 renderizador.render(escena,camara);
