6fea30e6306771dd8d197c5c63285d54501424e4
allanjmartell <allanjmartell@gmail.com>
2016-08-20 20:12:21 -0500

    Update ejercicio-2.js

--- rv/ejercicio-2.js
+++ rv/ejercicio-2.js
@@ -1,12 +1,12 @@
 var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=4;
+camara.position.z=7;
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
 var forma=new THREE.CylinderGeometry(.1,.1,2);
-var forma2=new THREE.SphereGeometry(.8,8,6);
+var forma2=new THREE.SphereGeometry(1,8,6);
 
 var material= new THREE.MeshNormalMaterial();
 
