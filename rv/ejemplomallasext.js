001ed3ddb9efadecb331eafd479247f32b364670
allanjmartell <allanjmartell@gmail.com>
2016-08-25 15:41:23 -0500

    Update ejemplomallasext.js

--- rv/ejemplomallasext.js
+++ rv/ejemplomallasext.js
@@ -11,12 +11,13 @@ var material=new THREE.MeshNormalMaterial();
 var malla=new THREE.Mesh(forma,material);
 malla.rotateX(Math.PI/4);
 
-var escena=new THREE.Scene();
+var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=500;
 
-var renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
