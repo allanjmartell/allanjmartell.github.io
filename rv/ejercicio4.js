51f7d70dd47911cf9d12bd80df4baef34afaa2ac
allanjmartell <allanjmartell@gmail.com>
2016-08-26 14:18:49 -0500

    Create ejercicio4.js

---
+++ rv/ejercicio4.js
@@ -0,0 +1,33 @@
+var estrella=new THREE.Shape();
+
+estrella.moveTo(-10,20);
+estrella.lineTo(-50,50);
+estrella.lineTo(-20,10);
+estrella.lineTo(-70,0);
+estrella.lineTo(-20,-10);
+estrella.lineTo(-50,-50);
+estrella.lineTo(-10,-20);
+estrella.lineTo(0,-70);
+estrella.lineTo(10,-20);
+estrella.lineTo(50,-50);
+estrella.lineTo(20,-10);
+estrella.lineTo(70,0);
+estrella.lineTo(20,10);
+estrella.lineTo(50,50);
+estrella.lineTo(10,20);
+estrella.lineTo(0,70);
+estrella.lineTo(-10,20);
+
+var forma= new THREE.ShapeGeometry(estrella);
+var malla= new THREE.Mesh(figura);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=50;
+
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
