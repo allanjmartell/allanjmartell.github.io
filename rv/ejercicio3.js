f0494fd7d7230f1cd834c3ad6107fdf13e977314
allanjmartell <allanjmartell@gmail.com>
2016-08-30 14:57:11 -0500

    Update ejercicio3.js

--- rv/ejercicio3.js
+++ rv/ejercicio3.js
@@ -34,8 +34,8 @@ var material=new THREE.MeshNormalMaterial();
 var malla=new THREE.Mesh(cubo,material);
 //malla.rotateX(Math.PI/4);
 //malla.rotateY(Math.PI/4);
-malla.rotateX(Math.PI/2);
-malla.rotateY(Math.PI/2);
+malla.rotateX(Math.PI/6);
+malla.rotateY(Math.PI/6);
 
 var escena=new THREE.Scene();
 escena.add(malla);
