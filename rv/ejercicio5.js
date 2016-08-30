fe7e63a6f4c136f183cb6ccbf6634dd67069d608
allanjmartell <allanjmartell@gmail.com>
2016-08-30 18:00:18 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -1,7 +1,7 @@
 var puntos=[];
 
 for (var i=0; i<5;i++){
-  puntos.push(new THREE.Vector2(Math.sin(i),Math.cos(i));
+  puntos.push(new THREE.Vector2(Math.sin(i),Math.cos(i)));
   }
 
 var forma= new THREE.LatheGeometry(puntos);
