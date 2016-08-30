930a8bcfb3e252b24c74e547be81b8350f86f37a
allanjmartell <allanjmartell@gmail.com>
2016-08-30 18:45:07 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -16,6 +16,7 @@ var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara= new THREE.PerspectiveCamera();
+camara.position.z=20;
 
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
