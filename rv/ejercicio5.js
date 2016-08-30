b29e13708a38fd6c5645f88d5638d14a965c866c
allanjmartell <allanjmartell@gmail.com>
2016-08-30 17:52:53 -0500

    Create ejercicio5.js

---
+++ rv/ejercicio5.js
@@ -0,0 +1,23 @@
+var puntos=[];
+
+for (var i=0; i<5;i++){
+  puntos.push(new THREE.Vector2(Math.sin(i),Math.cos(i));
+  }
+
+var forma= new THREE.LatheGeometry(puntos);
+
+var material= new THREE.MeshNormalMaterial();
+
+var malla= new THREE.Mesh(forma,material);
+malla.rotateX(Math.PI/6);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=10;
+
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
