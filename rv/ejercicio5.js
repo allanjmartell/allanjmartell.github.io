fadc1bdd389e932bb95429372778a2f13d01d89b
allanjmartell <allanjmartell@gmail.com>
2016-08-31 13:55:39 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -2,20 +2,20 @@ var torre= [];
 
 //Torre
 for (var i=0;i<300;i++){
-  torre.push(new THREE.Vector2(50,i));
+  torre.push(new THREE.Vector2(80,i));
 }
 var formatorre= new THREE.LatheGeometry(torre);
 var material= new THREE.MeshNormalMaterial();
 var malla= new THREE.Mesh(formatorre,material);
 
 //Base
-var base= new THREE.CylinderGeometry(100,100,30,10);
+var base= new THREE.CylinderGeometry(100,100,50,10);
 var mallabase= new THREE.Mesh(base,material);
 
 //Techo
-var techo= new THREE.CylinderGeometry(100,100,25,10);
-var techo2= new THREE.CylinderGeometry(50,50,50,10);
-var techo3= new THREE.CylinderGeometry(30,30,65,10);
+var techo= new THREE.CylinderGeometry(100,100,35,10);
+var techo2= new THREE.CylinderGeometry(50,50,60,10);
+var techo3= new THREE.CylinderGeometry(30,30,75,10);
 var mallatecho= new THREE.Mesh(techo,material);
 var mallatecho2= new THREE.Mesh(techo2,material);
 var mallatecho3= new THREE.Mesh(techo3,material);
@@ -43,9 +43,9 @@ estrella.lineTo(10,20);
 estrella.lineTo(0,70);
 estrella.lineTo(-10,20);
 
-var forma2= new THREE.ExtrudeGeometry(estrella,{amount:10});
+var forma2= new THREE.ExtrudeGeometry(estrella,{amount:20});
 var mallaestrella= new THREE.Mesh(forma2,material);
-mallaestrella.translateY(315);
+mallaestrella.translateY(330);
 mallaestrella.rotateX(Math.PI/2);
 
 var escena= new THREE.Scene();
