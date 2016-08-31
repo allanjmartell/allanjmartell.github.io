617954577f7472100a825cc2dda954eb5a3edfaa
allanjmartell <allanjmartell@gmail.com>
2016-08-31 10:42:43 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -1,19 +1,25 @@
-var puntos= [];
+var torre= [];
 
-for (var i=0;i<10;i++){
-  puntos.push(new THREE.Vector2(5,i));
+//Torre
+for (var i=0;i<20;i++){
+  torre.push(new THREE.Vector2(5,i));
 }
-
-var forma= new THREE.LatheGeometry(puntos);
+var formatorre= new THREE.LatheGeometry(torre);
 
 var material= new THREE.MeshNormalMaterial();
 
-var malla= new THREE.Mesh(forma,material);
+var malla= new THREE.Mesh(formatorre,material);
 malla.rotateX(Math.PI/6);
+
+//Base
+var base= new THREE.CylinderGeometry(15,15,5,10);
+
+var mallabase= new THREE.Mesh(base,material);
+mallabase.rotateX(Math.Pi/6);
 
 
 var escena= new THREE.Scene();
-escena.add(malla);
+escena.add(malla,mallabase);
 
 var camara= new THREE.PerspectiveCamera();
 camara.position.z=40;
