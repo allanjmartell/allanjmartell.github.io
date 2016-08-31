1e59db1ae0b9a20aad2fe154f2fbc63d855bd389
allanjmartell <allanjmartell@gmail.com>
2016-08-31 10:49:40 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -1,7 +1,7 @@
 var torre= [];
 
 //Torre
-for (var i=0;i<50;i++){
+for (var i=0;i<30;i++){
   torre.push(new THREE.Vector2(5,i));
 }
 var formatorre= new THREE.LatheGeometry(torre);
@@ -12,7 +12,7 @@ var malla= new THREE.Mesh(formatorre,material);
 malla.rotateX(Math.PI/6);
 
 //Base
-var base= new THREE.CylinderGeometry(15,15,5,10);
+var base= new THREE.CylinderGeometry(10,10,5,10);
 
 var mallabase= new THREE.Mesh(base,material);
 mallabase.rotateX(Math.PI/6);
