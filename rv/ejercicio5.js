396697214dc11ab17e93ce5fb9d46e6fca35d07a
allanjmartell <allanjmartell@gmail.com>
2016-09-01 15:28:14 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -4,24 +4,21 @@ var torre= [];
 for (var i=0;i<300;i++){
   torre.push(new THREE.Vector2(80,i));
 }
+
 var formatorre= new THREE.LatheGeometry(torre);
 var material= new THREE.MeshNormalMaterial();
-var malla= new THREE.Mesh(formatorre,material);
 
 //Base
 var base= new THREE.CylinderGeometry(100,100,50,10);
-var mallabase= new THREE.Mesh(base,material);
 
 //Techo
 var techo= new THREE.CylinderGeometry(100,100,35,10);
 var techo2= new THREE.CylinderGeometry(50,50,100,10);
 var techo3= new THREE.CylinderGeometry(30,30,135,10);
-var mallatecho= new THREE.Mesh(techo,material);
-var mallatecho2= new THREE.Mesh(techo2,material);
-var mallatecho3= new THREE.Mesh(techo3,material);
-mallatecho.translateY(300);
-mallatecho2.translateY(300);
-mallatecho3.translateY(300);
+
+techo.translateY(300);
+techo2.translateY(300);
+techo3.translateY(300);
 
 //Decoración
 var estrella=new THREE.Shape();
@@ -44,12 +41,48 @@ estrella.lineTo(0,70);
 estrella.lineTo(-10,20);
 
 var forma2= new THREE.ExtrudeGeometry(estrella,{amount:20});
-var mallaestrella= new THREE.Mesh(forma2,material);
-mallaestrella.translateY(330);
-mallaestrella.rotateX(Math.PI/2);
+
+forma2.translateY(330);
+forma2.rotateX(Math.PI/2);
+
+var malla= new THREE.Mesh(formatorre);//1
+var mallabase= new THREE.Mesh(base);//2
+var mallatecho= new THREE.Mesh(techo);//3
+var mallatecho2= new THREE.Mesh(techo2);//4
+var mallatecho3= new THREE.Mesh(techo3);//5
+var mallaestrella= new THREE.Mesh(forma2);//6
+
+//Cuerpo completo
+var torrefinal= new THREE.Geometry();
+torrefinal.merge(malla.geometry,malla.matrix);
+torrefinal.merge(mallabase.geometry,mallabase.matrix);
+
+var torrefinal2= new THREE.Geometry();
+torrefinal2.merge(mallatecho.geometry,mallatecho.matrix);
+torrefinal2.merge(mallatecho2.geometry,mallatecho2.matrix);
+
+var torrefinal3= new THREE.Geometry();
+torrefinal3.merge(mallatecho3.geometry,mallatecho3.matrix);
+torrefinal3.merge(mallaestrella.geometry,mallaestrella.matrix);
+
+var mallatorrefinal= new THREE.Mesh(torrefinal);
+var mallatorrefinal2= new THREE.Mesh(torrefinal2);
+var mallatorrefinal3= new THREE.Mesh(torrefinal3);
+
+var torrefinal4= new THREE.Geometry();
+torrefinal4.merge(mallatorrefinal.geometry,mallatorrefinal.matrix);
+torrefinal4.merge(mallatorrefinal2.geometry,mallatorrefinal2.matrix);
+
+var mallatorrefinal4= new THREE.Mesh(torrefinal4);
+
+var torrefinal5= new THREE.Geometry();
+torrefinal5.merge(mallatorrefinal3.geometry,mallatorrefinal3.matrix);
+torrefinal5.merge(mallatorrefinal4.geometry,mallatorrefinal4.matrix);
+
+var Torrefinal= new THREE.Mesh(torrefinal5,material);
 
 var escena= new THREE.Scene();
-escena.add(malla,mallabase,mallatecho,mallatecho2,mallatecho3,mallaestrella);
+escena.add(Torrefinal);
 
 var camara= new THREE.PerspectiveCamera();
 camara.position.z=900;
