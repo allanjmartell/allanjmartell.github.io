31ef40c9cc52c6c69dc802faa760598c7b641f3e
allanjmartell <allanjmartell@gmail.com>
2016-09-01 23:56:07 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -43,7 +43,7 @@ estrella.lineTo(-10,20);
 
 var forma2= new THREE.ExtrudeGeometry(estrella,{amount:20});
 forma2.rotateX(Math.PI/2);
-forma2.translate(0,300,0);
+forma2.translate(0,350,0);
 
 var malla= new THREE.Mesh(formatorre);//1
 var mallabase= new THREE.Mesh(base);//2
