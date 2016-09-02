0f9f38988f181ae0af3108a7b44709e76a63e557
allanjmartell <allanjmartell@gmail.com>
2016-09-01 23:04:57 -0500

    Update ejercicio5.js

--- rv/ejercicio5.js
+++ rv/ejercicio5.js
@@ -41,8 +41,6 @@ estrella.lineTo(0,70);
 estrella.lineTo(-10,20);
 
 var forma2= new THREE.ExtrudeGeometry(estrella,{amount:20});
-forma2.translate(0,0,-10);
-forma2.rotateX(Math.PI/2);
 
 var malla= new THREE.Mesh(formatorre);//1
 var mallabase= new THREE.Mesh(base);//2
@@ -51,6 +49,8 @@ var mallatecho2= new THREE.Mesh(techo2);//4
 var mallatecho3= new THREE.Mesh(techo3);//5
 var mallaestrella= new THREE.Mesh(forma2);//6
 
+mallaestrella.translateY(300);
+mallaestrella.rotateX(Math.PI/2);
 //Cuerpo completo
 var torrefinal= new THREE.Geometry();
 torrefinal.merge(malla.geometry,malla.matrix);
