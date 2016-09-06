637b30fdcfdee25866b0164c2e131e283f4b9cb0
allanjmartell <allanjmartell@gmail.com>
2016-09-06 15:10:20 -0500

    Create ejemplocamaraorto.js

---
+++ rv/ejemplocamaraorto.js
@@ -0,0 +1,35 @@
+var camara= new THREE.OrthographicCamera();
+
+camara.left= window.innerWidth/-2;
+camara.right= window.innerWidth/2;
+camara.top= window.innerHeight/2;
+camara.bottom= window.innerHeight/-2;
+camara.near= 0.1;
+camara.far= 1000;
+camara.updateProjectionMatrix();
+
+camara.position.z=100;
+
+var cubo= new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshNormalMaterial());
+
+cubo.rotateY(Math.PI/4);
+
+var esfera1= new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
+
+esfera1.position.x=5;
+
+var esfera2= new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
+
+esfera2.position.x=-5;
+esfera2.position.z=-10;
+
+var escena= new THREE.Scene();
+
+escena.add(esfera1,esfera2,cubo);
+
+var renderizador= new THREE.WebGLRenderer();
+
+renderizador.setSize(window.innerWidth,window.innerHeight);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
