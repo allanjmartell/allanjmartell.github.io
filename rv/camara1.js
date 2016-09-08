78da7ecae494cc22c212ce996b94aed15a15bb91
allanjmartell <allanjmartell@gmail.com>
2016-09-08 15:50:15 -0500

    Update camara1.js

--- rv/camara1.js
+++ rv/camara1.js
@@ -1,5 +1,5 @@
-var cubos=[];
 var mallacubo=[];
+var tablero=[];
 var b= new THREE.Color("rgb(255,255,255)");
 var g= new THREE.Color("rgb(130,130,130)");
 var blanco= new THREE.MeshBasicMaterial(b);
@@ -7,21 +7,28 @@ var negro= new THREE.MeshBasicMaterial(g);
 
 
 for (var j=1;j<=8;j++){
-   cubos.push(new THREE.BoxGeometry(10,10,10));
+   //Creación de geometria
+   var cubo= new THREE.BoxGeometry(10,10,10));
+   cubo.translate(j*10,0,0);
    var m=j%2;
+   //Creación de mallas
    if (m==0){
-      mallacubo[j]= new THREE.Mesh(cubos[j],blanco);
+      mallacubo[j]= new THREE.Mesh(cubo,blanco);
    }
    else{
-      mallacubo[j]= new THREE.Mesh(cubos[j],negro);}
-   mallacubo[j].translateX(j*10);
+      mallacubo[j]= new THREE.Mesh(cubo,negro);}
+}
+
+for (var j=1;j<=8;j++){
+ tablero[j]=new THREE.Geometry();
+   tablero[j-1].merge(mallacubo[j-1].geometry,mallacubo[j-1].matrix);
+   tablero[j].merge(mallacubo[j].geometry,mallacubo[j].matrix)
 }
 
 var escena=new THREE.Scene();
-escena.add(mallacubo);
+escena.add(tablero);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=500;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
