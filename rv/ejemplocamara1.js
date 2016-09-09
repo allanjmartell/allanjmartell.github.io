ec92b5e1930f7526aaa658373c11e933b2eb53b3
allanjmartell <allanjmartell@gmail.com>
2016-09-09 11:08:56 -0500

    Update ejemplocamara1.js

--- rv/ejemplocamara1.js
+++ rv/ejemplocamara1.js
@@ -12,6 +12,7 @@ var escena=new THREE.Scene();
 escena.add(malla);
 
 var camara=new THREE.PerspectiveCamera();
+camara.position.z=30;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
