2cadc37453ec87e82486eb5d25479705dacf6628
allanjmartell <allanjmartell@gmail.com>
2016-09-09 16:45:26 -0500

    Update ejemplocamara1.js

--- rv/ejemplocamara1.js
+++ rv/ejemplocamara1.js
@@ -1,11 +1,11 @@
 var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=100;
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
-var forma=new THREE.BoxGeometry(5,5,5);
+var forma=new THREE.BoxGeometry(10,10,10);
 var material= new THREE.MeshNormalMaterial();
 var cubo=new THREE.Mesh(forma,material);
 cubo.rotateX(Math.PI/4);
