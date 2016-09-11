c12eaffebb6cc336c3017c3c6fd9974232e81b14
allanjmartell <allanjmartell@gmail.com>
2016-09-11 16:44:18 -0500

    Update ejemplocamara1.js

--- rv/ejemplocamara1.js
+++ rv/ejemplocamara1.js
@@ -2,6 +2,7 @@ var escena=new THREE.Scene();
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=100;
+camara.position.y=20;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
@@ -35,12 +36,13 @@ for (var i=0;i<8;i++){
 grupo.rotateX(Math.PI/4);
 grupo.rotateY(Math.PI/4);
 
+//grupo2
 var grupo2= new THREE.Group();
 
 for(var l=0;l<10;l++){
   for(var m=0;m<2;m++){
   var malla2= new THREE.Mesh(cubo,mcafe);
-  if(m==1){malla2.position.z=(-100);}
+  if(m==1){malla2.position.z=(-90);}
   malla2.position.x=(l*10);
   malla2.matrixAutoUpdate = false;
   malla2.updateMatrix();
@@ -49,6 +51,22 @@ for(var l=0;l<10;l++){
 
 grupo2.rotateX(Math.PI/4);
 grupo2.rotateY(Math.PI/4);
+
+//grupo3
+var grupo3= new THREE.Group();
+
+for(var l=1;l<9;l++){
+  for(var m=0;m<2;m++){
+  var malla3= new THREE.Mesh(cubo,mcafe);
+  if(m==1){malla3.position.x=(90);}
+  malla3.position.z=(-l*10);
+  malla3.matrixAutoUpdate = false;
+  malla3.updateMatrix();
+  grupo3.add(malla3);
+}}
+
+grupo3.rotateX(Math.PI/4);
+grupo3.rotateY(Math.PI/4);
 
 escena.add(grupo,grupo2);
 renderizador.render(escena,camara);
