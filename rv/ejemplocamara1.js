483ac14e93fc7ccb4e982ad0d4494e233650b8ab
allanjmartell <allanjmartell@gmail.com>
2016-09-11 15:53:26 -0500

    Update ejemplocamara1.js

--- rv/ejemplocamara1.js
+++ rv/ejemplocamara1.js
@@ -8,15 +8,20 @@ renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
 var material= new THREE.MeshNormalMaterial();
-var cubo=[];
-var figura=[];
-for (var i=0;i<2;i++){
-cubo.push(new THREE.BoxGeometry());
-figura.push(new THREE.Mesh(cubo[i],material));
-}
+var cubo=new THREE.BoxGeometry(10,10,10);
+var grupo= new THREE.Group();
 
-//figura.rotateX(Math.PI/4);
+for (var i=0;i<8;i++){
+var malla= new THREE.Mesh(cubo,material);
+malla.position.x=i*10;
+
+malla.matrixAutoUpdate = false;
+malla.updateMatrix();
+
+grupo.add(malla);
+}
+grupo.rotateX(Math.PI/4);
 //figura.rotateY(Math.PI/4);
 
-escena.add(figura[1]);
+escena.add(grupo);
 renderizador.render(escena,camara);
