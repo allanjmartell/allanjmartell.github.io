b1a1119a8079658d5b19d469e9e1f9ca5f751c44
allanjmartell <allanjmartell@gmail.com>
2016-09-11 16:50:35 -0500

    Update ejemplocamara1.js

--- rv/ejemplocamara1.js
+++ rv/ejemplocamara1.js
@@ -8,7 +8,7 @@ var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 
-var textura = new THREE.TextureLoader().load( 'textures/crate.gif' );
+var textura = new THREE.TextureLoader().load( 'madera.jpg' );
 var cubo=new THREE.BoxGeometry(10,10,10);
 
 var mblanco= new THREE.MeshBasicMaterial({color: 0xffffff});
