8b85f63d0f336d3959e26f7920559b33751ab10f
allanjmartell <allanjmartell@gmail.com>
2016-09-11 16:27:21 -0500

    Update ejemplocamara1.js

--- rv/ejemplocamara1.js
+++ rv/ejemplocamara1.js
@@ -19,8 +19,8 @@ for (var i=0;i<8;i++){
   if(k%2==0){var malla= new THREE.Mesh(cubo,mnegro);}
   else{var malla= new THREE.Mesh(cubo,mblanco);}
 
-  malla.position.x=j*10;
-  malla.position.z=-i*10;
+  malla.position.x=(j+1)*10;
+  malla.position.z=(-i-1)*10;
 
   malla.matrixAutoUpdate = false;
   malla.updateMatrix();
@@ -34,5 +34,18 @@ for (var i=0;i<8;i++){
 grupo.rotateX(Math.PI/4);
 grupo.rotateY(Math.PI/4);
 
-escena.add(grupo);
+var grupo2= new THREE.Group();
+
+for(var i=0;i<=10;i++){
+  var malla2= new THREE.Mesh(cubo,mcafe);
+  malla2.position.x=(j*10);
+  malla2.matrixAutoUpdate = false;
+  malla2.updateMatrix();
+  grupo2.add(malla2);
+}
+
+grupo2.rotateX(Math.PI/4);
+grupo2.rotateY(Math.PI/4);
+
+escena.add(grupo,grupo2);
 renderizador.render(escena,camara);
