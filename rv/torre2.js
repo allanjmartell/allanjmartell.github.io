002fd692eb939676836c7f2634ed7948885c56a3
allanjmartell <allanjmartell@gmail.com>
2016-09-14 14:07:17 -0500

    Update torre2.js

--- rv/torre2.js
+++ rv/torre2.js
@@ -32,6 +32,17 @@ var cubito3= new THREE.BoxGeometry(3,4,9.5);
 cubito3.rotateY(Math.PI/3);
 cubito3.translate(3.5,33.5,-6);
 
+var cubito1= new THREE.BoxGeometry(3,4,9.5);
+cubito1.translate(7,33.5,0);
+
+var cubito22= new THREE.BoxGeometry(3,4,9.5);
+cubito22.rotateY(Math.PI/-3);
+cubito22.translate(-3.5,33.5,-6);
+
+var cubito33= new THREE.BoxGeometry(3,4,9.5);
+cubito33.rotateY(Math.PI/3);
+cubito33.translate(-3.5,33.5,6);
+
 //pico
 var pico= new THREE.ConeGeometry(3,4,10);
 pico.translate(0,38.7,0);
@@ -44,6 +55,9 @@ var mallatecho3= new THREE.Mesh(techo3);//5
 var mallaanillo= new THREE.Mesh(cubito);//6
 var mallacubito2= new THREE.Mesh(cubito2);
 var mallacubito3= new THREE.Mesh(cubito3);
+var mallacubito1= new THREE.Mesh(cubito1);
+var mallacubito22= new THREE.Mesh(cubito22);
+var mallacubito33= new THREE.Mesh(cubito33);
 var mallapico= new THREE.Mesh(pico);
 
 //Cuerpo completo
@@ -91,7 +105,25 @@ var torrefinal8= new THREE.Geometry();
 torrefinal8.merge(mallatorrefinal7.geometry,mallatorrefinal7.matrix);
 torrefinal8.merge(mallacubito3.geometry,mallacubito3.matrix);
 
-var Torrefinal= new THREE.Mesh(torrefinal8,material);
+var mallatorrefinal8= new THREE.Mesh(torrefinal8);
+
+var torrefinal9= new THREE.Geometry();
+torrefinal9.merge(mallatorrefinal8.geometry,mallatorrefinal8.matrix);
+torrefinal9.merge(mallacubito1.geometry,mallacubito1.matrix);
+
+var mallatorrefinal9= new THREE.Mesh(torrefinal9);
+
+var torrefinal10= new THREE.Geometry();
+torrefinal10.merge(mallatorrefinal9.geometry,mallatorrefinal9.matrix);
+torrefinal10.merge(mallacubito22.geometry,mallacubito22.matrix);
+
+var mallatorrefinal10= new THREE.Mesh(torrefinal10);
+
+var torrefinal11= new THREE.Geometry();
+torrefinal11.merge(mallatorrefinal10.geometry,mallatorrefinal10.matrix);
+torrefinal11.merge(mallacubito33.geometry,mallacubito33.matrix);
+
+var Torrefinal= new THREE.Mesh(torrefinal11,material);
 
 Torrefinal.rotateX(Math.PI/6);
 
