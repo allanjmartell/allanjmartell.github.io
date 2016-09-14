0d662c4dee10776b702f9ebc7c7e076cda8389ef
allanjmartell <allanjmartell@gmail.com>
2016-09-14 00:24:24 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -22,8 +22,7 @@ techo3.translate(0,30,0);
 
 //Decoración
 var cubito= new THREE.BoxGeometry(4,3,3);
-cubito.rotateX(Math.PI/4);
-cubito.translate(0,40,10);
+cubito.translate(-10,40,10);
 
 //pico
 var pico= new THREE.ConeGeometry(3,4,10);
