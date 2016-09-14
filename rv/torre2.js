05eae66cfb569838c175e3690f4e7eda3d3bb347
allanjmartell <allanjmartell@gmail.com>
2016-09-14 13:42:58 -0500

    Update torre2.js

--- rv/torre2.js
+++ rv/torre2.js
@@ -26,7 +26,7 @@ cubito.translate(-6,33.5,0);
 
 var cubito2= new THREE.BoxGeometry(3,4,9.5);
 cubito2.rotateY(Math.PI/-3);
-cubito2.translate(3.7,33.5,6);
+cubito2.translate(3.5,33.5,5.5);
 
 //pico
 var pico= new THREE.ConeGeometry(3,4,10);
