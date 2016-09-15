dae9fdb5fa489c47c834a5b0060ea5ae64bd0b56
allanjmartell <allanjmartell@gmail.com>
2016-09-14 22:57:57 -0500

    Update and rename torre.js to torrenegra.js

--- rv/torrenegra.js
+++ rv/torrenegra.js
@@ -7,6 +7,7 @@ for (var i=0;i<15;i++){
 
 var formatorre= new THREE.LatheGeometry(torre);
 //var material= new THREE.MeshNormalMaterial();
+var pnegro= new THREE.MeshBasicMaterial({color: 0x171714});//Pieza negra
 
 //Base
 var base= new THREE.CylinderGeometry(5,5,2.5,10);
@@ -123,4 +124,4 @@ var torrefinal11= new THREE.Geometry();
 torrefinal11.merge(mallatorrefinal10.geometry,mallatorrefinal10.matrix);
 torrefinal11.merge(mallacubito33.geometry,mallacubito33.matrix);
 
-var Torrefinal= new THREE.Mesh(torrefinal11);
+var Torrefinal= new THREE.Mesh(torrefinal11,pnegro);
