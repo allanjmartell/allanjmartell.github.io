d2cebbb642a1b58e776d0a8983f74f4ece3ad0ab
allanjmartell <allanjmartell@gmail.com>
2016-09-20 15:26:13 -0500

    Update ejercicio7.js

--- rv/ejercicio7.js
+++ rv/ejercicio7.js
@@ -68,7 +68,7 @@ torre1.position.y=5;
 torre1.position.z=-10;
 torre1.position.x=10;
 torre1.transparent(true);
-torre1.opacity:.5;
+torre1.opacity=.5;
 
 //Opacidad 100
 var torre2= Torrenegra2.clone();
