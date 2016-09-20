2503057040fa42833649a855b0ec2a14b5046fb3
allanjmartell <allanjmartell@gmail.com>
2016-09-20 15:18:01 -0500

    Update torreblanca.js

--- rv/torreblanca.js
+++ rv/torreblanca.js
@@ -8,6 +8,7 @@ for (var i=0;i<15;i++){
 var formatorre= new THREE.LatheGeometry(torre);
 //var material= new THREE.MeshNormalMaterial();
 var pblanco= new THREE.MeshBasicMaterial({color: 0xEEEED8});//Pieza blanca
+var pblanco2=new THREE.MeshLambertMaterial({color: 0xEEEED8});
 
 //Base
 var base= new THREE.CylinderGeometry(5,5,2.5,10);
@@ -125,3 +126,4 @@ torrefinal11.merge(mallatorrefinal10.geometry,mallatorrefinal10.matrix);
 torrefinal11.merge(mallacubito33.geometry,mallacubito33.matrix);
 
 var Torreblanca= new THREE.Mesh(torrefinal11,pblanco);
+var Torreblanca2=new THREE.Mesh(torrefinal11,pblanco2);
