800ba661ec0b9023b5be6d6432d072a5ccd96529
allanjmartell <allanjmartell@gmail.com>
2016-09-20 15:07:00 -0500

    Update ejercicio7.js

--- rv/ejercicio7.js
+++ rv/ejercicio7.js
@@ -3,7 +3,7 @@ escena.rotateX(Math.PI/4);
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=200;
-camara.position.x=200;
+camara.position.x=100;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
