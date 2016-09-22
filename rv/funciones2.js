df59918c3789f7a1f5621544a32d75baab53a290
allanjmartell <allanjmartell@gmail.com>
2016-09-22 15:35:05 -0500

    Create funciones2.js

---
+++ rv/funciones2.js
@@ -0,0 +1,32 @@
+function init(p){
+  malla= new THREE.Mesh(new THREE.BoxGeometry(p,p,p),new THREE.MeshNormalMaterial());
+  
+  escena= new THREE.Scene();
+  escena.add(malla);
+  
+  camara= new THREE.PerspectiveCamera();
+  camara.position.z=20;
+  camara.position.y=1;
+  renderizador= new THREE.WebGLRenderer();
+  document.body.appendChild(renderizador.domElement);
+  
+  renderizador.setSize(700,700);
+}
+
+var a=0;
+
+var loop= function(){ 
+  //p(1);**
+  requestAnimationFrame(loop);
+  renderizador.render(escena,camara);
+  
+  if(a==0){
+  malla.position.x=1+p;}
+  if(a==10){
+  malla.position.x=1-p;}
+}
+  
+var escena,camara,renderizador,malla;
+
+init(1);
+loop();
