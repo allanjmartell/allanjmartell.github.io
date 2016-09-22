4cc27979c96208cea3ddb642623552e8f1f271e3
allanjmartell <allanjmartell@gmail.com>
2016-09-22 15:10:58 -0500

    Update ejemplofunciones.js

--- rv/ejemplofunciones.js
+++ rv/ejemplofunciones.js
@@ -6,7 +6,7 @@ function init(p){
   
   camara= new THREE.PerspectiveCamera();
   camara.position.z=5;
-  camara.position.y=5;
+  camara.position.y=1;
   renderizador= new THREE.WebGLRenderer();
   document.body.appendChild(renderizador.domElement);
   
