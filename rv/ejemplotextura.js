0e06fe2c0798727a77a81faf0dfef0120845650e
allanjmartell <allanjmartell@gmail.com>
2016-09-27 15:33:50 -0500

    Create ejemplotextura.js

---
+++ rv/ejemplotextura.js
@@ -0,0 +1,32 @@
+var TEXTURA = new Object();
+
+TEXTURA.retrollamada = function(textura){
+  var material = new THREE.MeshBasicMaterial({map:textura});
+  TEXTURA.malla = new THREE.Mesh(new THREE.SphereGeometry(1),material);
+  TEXTURA.escena.add(TEXTURA.malla);
+}
+
+TEXTURA.setup = function(){
+  TEXTURA.escena = new THREE.Scene();
+  TEXTURA.cargador = new THREE.TextureLoader();
+  cargador.load("textura.jpg",TEXTURA.retrollamada);
+  
+  TEXTURA.camara = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+  TEXTURA.camara.position.z=5;
+  
+  var lienzo = document.getElementById("ejemplotextura");
+  TEXTURA.renderizador = new THREE.WebGLRenderer({canvas:lienzo,antialias:true});
+  TEXTURA.renderizador.setSize(600,600);
+}
+  
+TEXTURA.loop = function(){
+  requestAnimationFrame(TEXTURA.loop);
+  
+  if(TEXTURA.malla !==undefined){
+  TEXTURA.malla.rotateX(0.01);
+  TEXTURA.malla.rotateY(0.01);
+  }
+} 
+
+TEXTURA.setup();
+TEXTURA.loop();
