c7587def63e5d691b6d3ec9feaf68c83e551973b
allanjmartell <allanjmartell@gmail.com>
2016-10-02 17:14:51 -0500

    Update ejercicio8.js

--- rv/ejercicio8.js
+++ rv/ejercicio8.js
@@ -15,7 +15,9 @@ function init() {
   
   //Textura
   var textura1 = new THREE.TextureLoader().load('marmolblanco.jpg');
+  var textura2 = new THREE.TextureLoader().load('marmolnegro.jpg');
   var marmolblanco = new THREE.MeshBasicMaterial({map:textura1});
+  var marmolnegro = new THREE.MeshBasicMaterial({map:textura2});
   //Torre1
   torre1 = new THREE.Mesh(torrefinal11,marmolblanco);
   torre1.position.y=5;
@@ -26,28 +28,23 @@ function init() {
   torre2.position.y=5;
   torre2.position.z=-80;
   torre2.position.x=10;
-  
-  escena.add(torre1,torre2);
+  //Torre3
+  torre3 = new THREE.Mesh(torrefinal11,marmolnegro);
+  torre3.position.y=5;
+  torre3.z=-80;
+  torre3.position.x=80;
+  //torre4
+  torre4 = new THREE.Mesh(torrefinal11,marmolnegro);
+  torre4.position.y=5;
+  torre4.position.z=-10;
+  torre4.position.x=80;
+  escena.add(torre1,torre2,torre3,torre4);
   
   renderizador = new THREE.WebGLRenderer();
   renderizador.setPixelRatio( window.devicePixelRatio );
   renderizador.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild(renderizador.domElement);
 }
-
-//TEXTURA.retrollamada2 = function(textura2){
-  //var marmolnegro = new THREE.MeshBasicMaterial({map:textura2}); 
-  //Torre3
-  //TEXTURA.malla3 = new THREE.Mesh(torrefinal11,marmolnegro);
-  //TEXTURA.malla3.position.y=5;
-  //TEXTURA.malla3.z=-80;
-  //TEXTURA.malla3.position.x=80;
-  //torre4
-  //TEXTURA.malla4 = new THREE.Mesh(torrefinal11,marmolnegro);
-  //TEXTURA.malla4.position.y=5;
-  //TEXTURA.malla4.position.z=-10;
-  //TEXTURA.malla4.position.x=80;
-//}
 
 function loop() {
   requestAnimationFrame(loop);
