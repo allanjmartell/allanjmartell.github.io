1fd9d4e4b8e0be4cf7e8860623b79e56bff19b23
allanjmartell <allanjmartell@gmail.com>
2016-10-07 16:59:22 -0500

    Update ejercicio9.js

--- rv/ejercicio9.js
+++ rv/ejercicio9.js
@@ -190,8 +190,8 @@ prototipo.setup = function(){
     for (var i=0;i<8;i++){
       for(var j=0;j<8;j++){
 
-      if(k%2==0){malla1= new THREE.Mesh(new prototipo.TableroGeometry.cubo,ceranegro);}
-      else{malla1= new THREE.Mesh(new prototipo.TableroGeometry.cubo,cerablanco);}
+      if(k%2==0){malla1= new THREE.Mesh(new prototipo.TableroGeometry.cubo(),ceranegro);}
+      else{malla1= new THREE.Mesh(new prototipo.TableroGeometry.cubo(),cerablanco);}
 
       malla1.position.x=(j+1)*10;
       malla1.position.z=(-i-1)*10;
