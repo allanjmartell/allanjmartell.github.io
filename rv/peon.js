9aefed4d04964b2e39cc563d36651b39219248a4
allanjmartell <allanjmartell@gmail.com>
2016-10-08 14:03:04 -0500

    Update peon.js

--- rv/peon.js
+++ rv/peon.js
@@ -3,20 +3,26 @@ var basepeon = new THREE.CylinderGeometry(5,5,2,10);//Altura = 2
 var basepeon2= new THREE.TorusGeometry(3,2,20,100);//Altura = 4
 basepeon2.rotateX(Math.PI/2);
 basepeon2.translate(0,2,0);
+var columna = new THREE.CylinderGeometry(3.5,3.5,8,10);
+columna.translate(0,2,0);
 
 //Mesh
 var mbasepeon = new THREE.Mesh(basepeon);
 var mbasepeon2= new THREE.Mesh(basepeon2);
+var mcolumna = new THREE.Mesh(columna);
 
 //Acoplamiento
 var peonfinal = new THREE.Geometry();
 peonfinal.merge(mbasepeon.geometry,mbasepeon.matrix);
 peonfinal.merge(mbasepeon2.geometry,mbasepeon2.matrix);
 
+var peonfinal2 = new THREE.Geometry();
+peonfinal2.merge(mcolumna.geometry,mcolumna.matrix);
+peonfinal2.merge(peonfinal.geometry,peonfinal.matrix);
 
 //Material
 var material= new THREE.MeshNormalMaterial();
-var peon= new THREE.Mesh(peonfinal,material);
+var peon= new THREE.Mesh(peonfinal2,material);
 
 var escena,camara,renderizador;
 escena = new THREE.Scene();
