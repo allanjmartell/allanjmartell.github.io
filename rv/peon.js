9bb11c4515a900930988566841f082919949e4d4
allanjmartell <allanjmartell@gmail.com>
2016-10-08 14:10:50 -0500

    Update peon.js

--- rv/peon.js
+++ rv/peon.js
@@ -5,11 +5,13 @@ basepeon2.rotateX(Math.PI/2);
 basepeon2.translate(0,2,0);
 var columna = new THREE.CylinderGeometry(3.5,3.5,8,10);
 columna.translate(0,6,0);
-
+var techopeon= new THREE.CylinderGeometry(4,4,1,10);//Altura = 9
+techopeon.translate(0,10,0);
 //Mesh
 var mbasepeon = new THREE.Mesh(basepeon);
 var mbasepeon2= new THREE.Mesh(basepeon2);
 var mcolumna = new THREE.Mesh(columna);
+var mtechopeon= new THREE.Mesh(techopeon);
 
 //Acoplamiento
 var peonfinal = new THREE.Geometry();
@@ -20,17 +22,22 @@ var mpeonfinal = new THREE.Mesh(peonfinal);
 var peonfinal2 = new THREE.Geometry();
 peonfinal2.merge(mcolumna.geometry,mcolumna.matrix);
 peonfinal2.merge(mpeonfinal.geometry,mpeonfinal.matrix);
+var mpeonfinal2= new THREE.Mesh(peonfinal2);
+
+var peonfinal3 = new THREE.Geometry();
+peonfinal2.merge(mtechopeon.geometry,mtechopeon.matrix);
+peonfinal2.merge(mpeonfinal2.geometry,mpeonfinal2.matrix);
 
 //Material
 var material= new THREE.MeshNormalMaterial();
-var peon= new THREE.Mesh(peonfinal2,material);
+var peon= new THREE.Mesh(peonfinal3,material);
 
 var escena,camara,renderizador;
 escena = new THREE.Scene();
 //escena.rotateX(Math.PI/4);
 //Camara
 camara = new THREE.PerspectiveCamera();
-camara.position.z=20;
+camara.position.z=30;
  
 escena.add(peon);
   
