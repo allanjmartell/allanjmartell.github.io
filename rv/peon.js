5db95c4389bd636e1aa0f2d1e342a7152fccf935
allanjmartell <allanjmartell@gmail.com>
2016-10-08 14:20:30 -0500

    Update peon.js

--- rv/peon.js
+++ rv/peon.js
@@ -1,6 +1,6 @@
 //Geometrias
 var basepeon = new THREE.CylinderGeometry(5,5,2,20);//Altura = 2
-var basepeon2= new THREE.TorusGeometry(3,2,20,100);//Altura = 4
+var basepeon2= new THREE.TorusGeometry(3,1,20,100);//Altura = 4
 basepeon2.rotateX(Math.PI/2);
 basepeon2.translate(0,2,0);
 var columna = new THREE.CylinderGeometry(3.5,3.5,8,10);
