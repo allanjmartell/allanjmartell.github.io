0adfac97b63471a05e7a553451d051c350ddec33
allanjmartell <allanjmartell@gmail.com>
2016-10-20 15:13:07 -0500

    Update ejemploAnimacion.js

--- rv/ejemploAnimacion.js
+++ rv/ejemploAnimacion.js
@@ -20,6 +20,7 @@ function setup(){
   escena = new THREE.Scene();
   escena.add(pieza);
   escena.rotateY(Math.PI/4);
+  escena.rotateX(Math.PI/2);
   camara= new THREE.PerspectiveCamera();
   camara.position.z=30;
   camara.position.y=5;
