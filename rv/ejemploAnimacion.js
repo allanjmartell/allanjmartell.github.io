f85b3d60a5c6fe2cd5483ff0c5f8f8032d7e2ed3
allanjmartell <allanjmartell@gmail.com>
2016-10-20 15:01:12 -0500

    Create ejemploAnimacion.js

---
+++ rv/ejemploAnimacion.js
@@ -0,0 +1,36 @@
+function Pieza(){
+  THREE.Object3D.call(this);
+  this.piernaIzq = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+  this.piernaDer = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+  var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
+  this.add(piernaIzq,piernaDer,cuerpo);
+  this.piernaIzq.position.z=-2;
+  this.piernaIzq.position.y=-2.5;
+  this.piernaDer.position.z=2;
+  this.piernaDer.position.y=-2.5;
+  cuerpo.position.z=2.5;
+}
+
+var pieza,escena,camara,renderizador;
+
+Pieza.prototype = new THREE.Object3D;
+
+function setup(){
+  pieza = new Pieza();
+  escena = new THREE.Scene();
+  escena.add(pieza);
+  camara= new THREE.PerspectiveCamera();
+  camara.position.z=20;
+  renderizador = new THREE.WebGLRenderer();
+  renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+  document.body.appendChild(renderizador.domElement);
+}
+
+function loop(){
+  pieza.rotateY=0.1;
+  requestAnimationFrame(loop);
+  renderizador.render(escena,camara);
+}
+
+setup();
+loop();
