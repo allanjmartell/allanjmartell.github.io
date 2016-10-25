04398a63374f73463b3de0fcbbcd76fce1462be9
allanjmartell <allanjmartell@gmail.com>
2016-10-25 15:52:32 -0500

    Update ejercicioAgente.js

--- rv/ejercicioAgente.js
+++ rv/ejercicioAgente.js
@@ -98,6 +98,7 @@ function setup(){
   entorno.add(new Pared(1,7,-1));
   entorno.add(new Pared(1,-7,-1));
   entorno.add(new Pelota(0.5));
+  entorno.add(new Pelota(1));
   entorno.add(camara);
   
   renderer = new THREE.WebGLRenderer();
