1b4a924ed348f8f771801f00e63dd249789ecb9b
allanjmartell <allanjmartell@gmail.com>
2016-11-08 15:55:48 -0600

    Update proyectoFinal.js

--- rv/proyectoFinal.js
+++ rv/proyectoFinal.js
@@ -1,3 +1,48 @@
+function Agent(x=0,y=0){
+  THREE.Object3D.call(this);
+  this.position.x=x;
+  this.position.y=y;
+}
+
+Agent.prototype = new THREE.Object3D();
+
+//Las tres primitivas esenciales de un agente:
+Agent.prototype.sense = function(environment) {}; //Percibir
+Agent.prototype.plan = function(environment) {}; //Planificar
+Agent.prototype.act = function(environment) {}; //Actuar
+
+//Un Agente opera sobre un entorno
+function Environment(){
+  THREE.Scene.call(this);
+}
+
+Environment.prototype = new THREE.Scene();
+
+//Preguntar a todos los agentes si sienten
+Environment.prototype.sense = function(){ 
+  for(var i=0; i<this.children.length; i++){
+    if(this.children[i].sense!==undefined)
+      this.children[i].sense(this);
+  }
+}
+
+//Preguntar a todos los agentes si planean
+Environment.prototype.plan = function(){
+  for(var i=0; i<this.children.length; i++){
+    if(this.children[i].plan !== undefined)
+      this.children[i].plan(this);
+  }
+}
+
+//Preguntar a todos los agentes si actuan
+Environment.prototype.act = function(){
+  for(var i=0; i<this.children.length; i++){
+    if(this.children[i].act !== undefined)
+      this.children[i].act(this);
+  }
+}
+
+
 var camara,escena,renderizador;
 var torre1,torre2,torre3,torre4,malla,malla2,malla3,grupo,grupo2,grupo3;
 
