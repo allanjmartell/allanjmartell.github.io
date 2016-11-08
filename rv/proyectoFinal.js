ee623cdbe36014cae37d5152cf63e0aa67577d9c
allanjmartell <allanjmartell@gmail.com>
2016-11-08 15:36:49 -0600

    Update proyectoFinal.js

--- rv/proyectoFinal.js
+++ rv/proyectoFinal.js
@@ -17,9 +17,9 @@ function init() {
   var luzblan2=new THREE.PointLight(0xFFFFFF);
   var luzblan3= new THREE.PointLight(0xFFFFFF);
   
-  luzblan.position.y=150; luzblan.position.z=50; luzblan.position.x=-50;
-  luzblan2.position.y=150;  luzblan2.position.z=-150; luzblan2.position.x=50;
-  luzblan3.position.y=150;  luzblan3.position.z=50;  luzblan3.position.x=150
+  luzblan.position.y=200; luzblan.position.z=50; luzblan.position.x=-50;
+  luzblan2.position.y=200;  luzblan2.position.z=-150; luzblan2.position.x=50;
+  luzblan3.position.y=200;  luzblan3.position.z=50;  luzblan3.position.x=150
   //Textura
   var textura1 = new THREE.TextureLoader().load('marmolblanco.jpg');
   var textura2 = new THREE.TextureLoader().load('marmolnegro.jpg');
