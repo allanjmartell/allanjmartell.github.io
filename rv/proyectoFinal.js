67147695ba1215709c8d76ff4401fed5956f1f9e
allanjmartell <allanjmartell@gmail.com>
2016-12-03 17:47:51 -0600

    Update proyectoFinal.js

--- rv/proyectoFinal.js
+++ rv/proyectoFinal.js
@@ -281,11 +281,11 @@ function loop() {
 		 escena.remove(bloqueverde);
 		 if (torreblanca1.position.x===bloquerojo.position.x && torreblanca1.position.z===bloquerojo.position.z)
 			{TorreBlanca.prototype.act = function(environment){
-				if(torreblanca1.position.x<bloqueverde.position.x)
+				if(torreblanca1.position.x<=bloqueverde.position.x)
 				  {this.position.x += this.step;}
 				else
 				  {this.position.x -= this.step;}
-				if(torreblanca1.position.z<bloqueverde.position.z)
+				if(torreblanca1.position.z<=bloqueverde.position.z)
 				  {this.position.z += this.step;}
 				else
 				  {this.position.z -= this.step;}
