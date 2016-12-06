be08106b6672fcb42b1e55b16feb9cd69dc35cc3
allanjmartell <allanjmartell@gmail.com>
2016-12-06 15:53:24 -0600

    Update proyectoFinal.js

--- rv/proyectoFinal.js
+++ rv/proyectoFinal.js
@@ -100,9 +100,9 @@ function PeonBlanco(x=0,y=0,z=0){
     this.actuator = new THREE.Mesh( geometry, marmolblanco);
     this.actuator.overdraw = true;
     this.actuator.commands = [];
-    this.position.x=x;
-    this.position.y=y;
-    this.position.z=z;
+    this.actuator.position.x=x;
+    this.actuator.position.y=y;
+    this.actuator.position.z=z;
     escena.add( this.actuator );
   };
 	
