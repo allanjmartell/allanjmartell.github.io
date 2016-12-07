fd78875b04763cffa4ebdf956fcdef680138203b
allanjmartell <allanjmartell@gmail.com>
2016-12-06 21:49:05 -0600

    Update proyectoFinal.js

--- rv/proyectoFinal.js
+++ rv/proyectoFinal.js
@@ -92,25 +92,21 @@ TorreNegra.prototype = new Agent();
 ///////////////////////////////////////Peones//////////////////////////////////////////////////////////////////////////////////////////
 function PeonBlanco(x=0,y=0,z=0){
   Agent.call(this,x,y,z);
-  this.actuator = new THREE.JSONLoader();
+  var loader = new THREE.JSONLoader();
   var createMesh = function( geometry )
   {
     var textura1 = new THREE.TextureLoader().load('marmolblanco.jpg');
     var marmolblanco = new THREE.MeshLambertMaterial({map:textura1});
-    var malla = new THREE.Mesh( geometry, marmolblanco);
-//    this.actuator.overdraw = true;
-//    this.actuator.commands = [];
-    malla.position.x=x;
-    malla.position.y=y;
-    malla.position.z=z;
+    this.actuator = new THREE.Mesh( geometry, marmolblanco);
+    this.actuator.overdraw = true;
+    this.actuator.commands = [];
+    this.add(this.actuator);
+    this.position.x=x;
+    this.position.y=y;
+    this.position.z=z;
 //    escena.add( this.actuator );
 };
-  this.actuator.load( "peon2.js", createMesh );
-  this.add(this.actuator);
-  this.actuator.commands = [];
-  //this.actuator.position.x=x;
-  //this.actuator.position.y=y;
-  //this.actuator.position.z=z;
+  loader.load( "peon2.js", createMesh );
   this.sensor = new Sensor();
 }
 
