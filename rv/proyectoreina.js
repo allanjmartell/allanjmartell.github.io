03df86a8ae60840a26751cde4d2ece274c76bf7d
allanjmartell <allanjmartell@gmail.com>
2016-12-07 15:27:06 -0600

    Create proyectoreina.js

---
+++ rv/proyectoreina.js
@@ -0,0 +1,47 @@
+var base = new THREE.CylinderGeometry(5,5,3,50);
+var material = new THREE.MeshNormalMaterial();
+
+var columna1 = new THREE.CylinderGeometry(3.5,4,2,50);
+columna1.translate(0,2.5,0);
+
+var toroide1 = new THREE.TorusGeometry(3.5,.5,50,50);
+toroide1.rotateX(Math.PI/2);
+toroide1.translate(0,3.5,0);
+
+//Mallas
+var mbase = new THREE.Mesh(base);
+var mcolumna1 = new THREE.Mesh(columna1);
+var mtoroide1 = new THREE.Mesh(toroide1);
+
+//Cuerpo completo
+
+var alfilfinal = new THREE.Geometry();
+alfilfinal.merge(mbase.geometry,mbase.matrix);
+alfilfinal.merge(mcolumna1.geometry,mcolumna1.matrix);
+
+var malfilfinal = new THREE.Mesh(alfilfinal);
+
+var alfilfinal2 = new THREE.Geometry();
+alfilfinal2.merge(malfilfinal.geometry,malfilfinal.matrix);
+alfilfinal2.merge(mcolumna1.geometry,mcolumna1.matrix);
+
+var malfilfinal2 = new THREE.Mesh(alfilfinal2);
+
+var alfilfinal3 = new THREE.Geometry();
+alfilfinal3.merge(malfilfinal2.geometry,malfilfinal.matrix);
+alfilfinal3.merge(mtoroide1.geometry,mtoroide1.matrix);
+
+var malfilfinal3 = new THREE.Mesh(alfilfinal3);
+
+
+var escena=new THREE.Scene();
+escena.add(malfilfinal3);
+escena.rotateX(Math.PI/6);
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=45;
+
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
