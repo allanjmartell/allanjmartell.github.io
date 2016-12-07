5ff1ec38aac733f93e13a1f686323f69496d5d57
allanjmartell <allanjmartell@gmail.com>
2016-12-07 14:22:06 -0600

    Update proyectoalfil.js

--- rv/proyectoalfil.js
+++ rv/proyectoalfil.js
@@ -2,7 +2,7 @@ var base = new THREE.CylinderGeometry(5,5,3,10);
 var material = new THREE.MeshNormalMaterial();
 
 var columna1 = new THREE.CylinderGeometry(3.5,4,1.5);
-columna1.translate(0,1.5,0);
+columna1.translate(0,3,0);
 
 var toroide1 = new THREE.TorusGeometry(3.5,1,10,10);
 toroide1.translate(0,4,0);
