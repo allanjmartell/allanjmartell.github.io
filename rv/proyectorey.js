b0fba984cbee27f54cc3379f278efea02218056f
allanjmartell <allanjmartell@gmail.com>
2016-12-07 18:35:56 -0600

    Update proyectorey.js

--- rv/proyectorey.js
+++ rv/proyectorey.js
@@ -46,7 +46,7 @@ forma.lineTo(-3,7);
 forma.lineTo(-1,1);
 
 var cruz= new THREE.ExtrudeGeometry(forma,{amount:2});
-cruz.scale(2,2,2);
+cruz.scale(.25,.25,.25);
 cruz.translate(0,21.25,0);
 
 //Mallas
