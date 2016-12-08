b2e7d2e7a157f68bc5f42f3275fab22ea5759c90
allanjmartell <allanjmartell@gmail.com>
2016-12-07 19:18:27 -0600

    Update proyectoFinal.js

--- rv/proyectoFinal.js
+++ rv/proyectoFinal.js
@@ -96,7 +96,7 @@ function PeonBlanco(x=0,y=0,z=0){
   Agent.call(this,x,y,z);
   var textura1 = new THREE.TextureLoader().load('maderablanca.jpg');
   var maderablanca = new THREE.MeshLambertMaterial({map:textura1});
-  this.actuator = new THREE.Mesh(peonfinal4,maderablanca);
+  this.actuator = new THREE.Mesh(peonfinal6,maderablanca);
   this.actuator.commands = [];
   this.add(this.actuator);
   this.position.y=y;
@@ -109,7 +109,7 @@ function PeonNegro(x=0,y=0,z=0){
   Agent.call(this,x,y,z);
   var textura2 = new THREE.TextureLoader().load('maderanegra.jpg');
   var maderanegra = new THREE.MeshLambertMaterial({map:textura2});
-  this.actuator = new THREE.Mesh(peonfinal4,maderanegra);
+  this.actuator = new THREE.Mesh(peonfinal6,maderanegra);
   this.actuator.commands = [];
   this.add(this.actuator);
   this.position.y=y;
