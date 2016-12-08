5b65443b1d61ad563663effe97fe6d4c51504a29
allanjmartell <allanjmartell@gmail.com>
2016-12-07 18:59:24 -0600

    Update proyectopeon.js

--- rv/proyectopeon.js
+++ rv/proyectopeon.js
@@ -1,37 +1,46 @@
-  //Geometrias
-  var basepeon = new THREE.CylinderGeometry(5,5,2,20);//Altura = 2
-  var basepeon2= new THREE.TorusGeometry(3,1.5,20,100);//Altura = 4
-  basepeon2.rotateX(Math.PI/2);
-  basepeon2.translate(0,1,0);
-  var columna = new THREE.CylinderGeometry(3.5,3.5,8,10);
-  columna.translate(0,6,0);
-  var techopeon= new THREE.CylinderGeometry(4,4,1,20);//Altura = 9
-  techopeon.translate(0,10,0);
-  var techopeon2=new THREE.SphereGeometry(3.5,20,20);
-  techopeon2.translate(0,12,0);
-  //Mesh
-  var mbasepeon = new THREE.Mesh(basepeon);
-  var mbasepeon2= new THREE.Mesh(basepeon2);
-  var mcolumna = new THREE.Mesh(columna);
-  var mtechopeon= new THREE.Mesh(techopeon);
-  var mtechopeon2=new THREE.Mesh(techopeon2);
+var base = new THREE.CylinderGeometry(5,5,3,50);
+var material = new THREE.MeshNormalMaterial();
 
-  //Acoplamiento
-  var peonfinal = new THREE.Geometry();
-  peonfinal.merge(mbasepeon.geometry,mbasepeon.matrix);
-  peonfinal.merge(mbasepeon2.geometry,mbasepeon2.matrix);
-  var mpeonfinal = new THREE.Mesh(peonfinal);
+var columna1 = new THREE.CylinderGeometry(3.5,4,2,50);
+columna1.translate(0,2.5,0);
 
-  var peonfinal2 = new THREE.Geometry();
-  peonfinal2.merge(mcolumna.geometry,mcolumna.matrix);
-  peonfinal2.merge(mpeonfinal.geometry,mpeonfinal.matrix);
-  var mpeonfinal2= new THREE.Mesh(peonfinal2);
+var toroide1 = new THREE.TorusGeometry(3.5,.5,50,50);
+toroide1.rotateX(Math.PI/2);
+toroide1.translate(0,3.5,0);
 
-  var peonfinal3 = new THREE.Geometry();
-  peonfinal3.merge(mtechopeon.geometry,mtechopeon.matrix);
-  peonfinal3.merge(mpeonfinal2.geometry,mpeonfinal2.matrix);
-  var mpeonfinal3= new THREE.Mesh(peonfinal3);
+//Mallas
+var mbase = new THREE.Mesh(base);
+var mcolumna1 = new THREE.Mesh(columna1);
+var mtoroide1 = new THREE.Mesh(toroide1);
 
-  var peonfinal4 = new THREE.Geometry();
-  peonfinal4.merge(mtechopeon2.geometry,mtechopeon2.matrix);
-  peonfinal4.merge(mpeonfinal3.geometry,mpeonfinal3.matrix);
+//Cuerpo completo
+
+var peonfinal = new THREE.Geometry();
+peonfinal.merge(mbase.geometry,mbase.matrix);
+peonfinal.merge(mcolumna1.geometry,mcolumna1.matrix);
+
+var mpeonfinal = new THREE.Mesh(peonfinal);
+
+var peonfinal2 = new THREE.Geometry();
+peonfinal2.merge(mpeonfinal.geometry,mpeonfinal.matrix);
+peonfinal2.merge(mcolumna1.geometry,mcolumna1.matrix);
+
+var mpeonfinal2 = new THREE.Mesh(peonfinal2);
+
+var peonfinal3 = new THREE.Geometry();
+peonfinal3.merge(mpeonfinal2.geometry,mpeonfinal2.matrix);
+peonfinal3.merge(mtoroide1.geometry,mtoroide1.matrix);
+
+var mpeonfinal3 = new THREE.Mesh(peonfinal3);
+
+var escena=new THREE.Scene();
+escena.add(mpeonfinal3);
+escena.rotateX(Math.PI/6);
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=70;
+
+var renderizador=new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
