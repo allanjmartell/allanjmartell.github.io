3f13ad0d562389c0eb61df22e45af093b3a8ab47
allanjmartell <allanjmartell@gmail.com>
2016-12-07 19:12:02 -0600

    Update proyectopeon.js

--- rv/proyectopeon.js
+++ rv/proyectopeon.js
@@ -14,12 +14,16 @@ columna2.translate(0,6.5,0);
 var btecho = new THREE.CylinderGeometry(1.5,3.5,2,50);
 btecho.translate(0,10.5,0);
 
+var bola = new THREE.SphereGeometry(2,50,50);
+bola.translate(0,12.4,0)
+
 //Mallas
 var mbase = new THREE.Mesh(base);
 var mcolumna1 = new THREE.Mesh(columna1);
 var mtoroide1 = new THREE.Mesh(toroide1);
 var mcolumna2 = new THREE.Mesh(columna2);
 var mbtecho = new THREE.Mesh(btecho);
+var mbola = new THREE.Mesh(bola);
 
 //Cuerpo completo
 
@@ -51,10 +55,16 @@ var peonfinal5 = new THREE.Geometry();
 peonfinal5.merge(mpeonfinal4.geometry,mpeonfinal4.matrix);
 peonfinal5.merge(mbtecho.geometry,mbtecho.matrix);
 
-var mpeonfinal5 = new THREE.Mesh(peonfinal5,material);
+var mpeonfinal5 = new THREE.Mesh(peonfinal5);
+
+var peonfinal6 = new THREE.Geometry();
+peonfinal6.merge(mpeonfinal5.geometry,mpeonfinal5.matrix);
+peonfinal6.merge(mbola.geometry,mbola.matrix);
+
+var mpeonfinal6 = new THREE.Mesh(peonfinal6,material);
 
 var escena=new THREE.Scene();
-escena.add(mpeonfinal5);
+escena.add(mpeonfinal6);
 escena.rotateX(Math.PI/6);
 
 var camara=new THREE.PerspectiveCamera();
