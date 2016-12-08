9c925ef8d124949eec76ba7f8721ac7b3bda1e67
allanjmartell <allanjmartell@gmail.com>
2016-12-07 19:03:04 -0600

    Update proyectopeon.js

--- rv/proyectopeon.js
+++ rv/proyectopeon.js
@@ -8,10 +8,16 @@ var toroide1 = new THREE.TorusGeometry(3.5,.5,50,50);
 toroide1.rotateX(Math.PI/2);
 toroide1.translate(0,3.5,0);
 
+var columna2 = new THREE.CylinderGeometry(2,3.5,6,50);
+columna2.translate(0,6.5,0);
+
+
+
 //Mallas
 var mbase = new THREE.Mesh(base);
 var mcolumna1 = new THREE.Mesh(columna1);
 var mtoroide1 = new THREE.Mesh(toroide1);
+var mcolumna2 = new THREE.Mesh(columna2);
 
 //Cuerpo completo
 
@@ -33,12 +39,18 @@ peonfinal3.merge(mtoroide1.geometry,mtoroide1.matrix);
 
 var mpeonfinal3 = new THREE.Mesh(peonfinal3);
 
+var peonfinal4 = new THREE.Geometry();
+peonfinal4.merge(mpeonfinal3.geometry,mpeonfinal3.matrix);
+peonfinal4.merge(mcolumna2.geometry,mcolumna2.matrix);
+
+var mpeonfinal4 = new THREE.Mesh(peonfinal4,material);
+
 var escena=new THREE.Scene();
-escena.add(mpeonfinal3);
+escena.add(mpeonfinal4);
 escena.rotateX(Math.PI/6);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=70;
+camara.position.z=40;
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
