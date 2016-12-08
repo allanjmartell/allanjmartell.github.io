d527365d077fce35585a73145c343e9a65c43c23
allanjmartell <allanjmartell@gmail.com>
2016-12-07 19:37:09 -0600

    Update proyectotorre.js

--- rv/proyectotorre.js
+++ rv/proyectotorre.js
@@ -12,31 +12,38 @@ var columna2 = new THREE.CylinderGeometry(2,3.5,10,50);
 columna2.translate(0,8.5,0);
 
 //Mallas
-var mcolumna1= new THREE.Mesh(columna1);//1
-var mtoroide1= new THREE.Mesh(toroide1);//2
-var mcolumna2= new THREE.Mesh(columna2);//3
+var mbase = new THREE.Mesh(base);
+var mcolumna1 = new THREE.Mesh(columna1);//1
+var mtoroide1 = new THREE.Mesh(toroide1);//2
+var mcolumna2 = new THREE.Mesh(columna2);//3
 
 //Cuerpo completo
-var torrefinal= new THREE.Geometry();
+var torrefinal = new THREE.Geometry();
 torrefinal.merge(mcolumna1.geometry,mcolumna1.matrix);
 torrefinal.merge(mtoroide1.geometry,mtoroide1.matrix);
 
-var mtorrefinal= new THREE.Mesh(torrefinal);
+var mtorrefinal = new THREE.Mesh(torrefinal);
 
-var torrefinal2= new THREE.Geometry();
+var torrefinal2 = new THREE.Geometry();
 torrefinal2.merge(mtorrefinal.geometry,mtorrefinal.matrix);
 torrefinal2.merge(mcolumna2.geometry,mcolumna2.matrix);
 
-var mtorrefinal2= new THREE.Mesh(torrefinal2,material);
+var mtorrefinal2 = new THREE.Mesh(torrefinal2);
 
-var escena=new THREE.Scene();
-escena.add(mtorrefinal2);
+var torrefinal3 = new THREE.Geometry();
+torrefinal3.merge(mtorrefinal2.geometry,mtorrefinal2.matrix);
+torrefinal3.merge(mbase.geometry,mbase.matrix);
+
+var mtorrefinal3 = new THREE.Mesh(torrefinal3,material);
+
+var escena = new THREE.Scene();
+escena.add(mtorrefinal3);
 escena.rotateX(Math.PI/6);
 
-var camara=new THREE.PerspectiveCamera();
+var camara = new THREE.PerspectiveCamera();
 camara.position.z=70;
 
-var renderizador=new THREE.WebGLRenderer();
+var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
